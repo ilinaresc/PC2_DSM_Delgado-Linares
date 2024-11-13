25ffb441199b3bc2147bc217fcbd38b4de442a12
DANIEL DELGADO <danieldd.93@gmail.com>
2024-11-13 11:44:46 -0500

    Cambios en mi-rama

---
+++ app/src/main/java/com/example/pc02_dsm_delgado_linares/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.pc02_dsm_delgado_linares
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.pc02_dsm_delgado_linares.ui.theme.PC02_DSM_DelgadoLinaresTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            PC02_DSM_DelgadoLinaresTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    PC02_DSM_DelgadoLinaresTheme {
+        Greeting("Android")
+    }
+}
---
+++ app/src/main/java/com/example/pc02_dsm_delgado_linares/ui/home/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.pc02_dsm_delgado_linares.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    val text: LiveData<String> = _text
+}
